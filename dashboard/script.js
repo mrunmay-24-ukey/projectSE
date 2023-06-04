1e447e2bacf708c9e1616c794087c58bc56a4e98
mrunmay-24-ukey <playscore07@gmail.com>
2023-06-04 18:18:41 +0530

    second commit

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,132 +1,76 @@
-// JS
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
-var ctx = document.getElementById('recentResultsChart').getContext('2d');
-var gradientFill = ctx.createLinearGradient(0, 0, 0, 200);
-gradientFill.addColorStop(0.1, "rgba(109,110,227, .3)");
-gradientFill.addColorStop(1, "rgba(255,255,255, .3)");
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
 
-
-var data = {
-    labels: ["", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", ""],
-    datasets: [{
-        backgroundColor: "rgba(0,0,0,0)",
-        borderColor: "#6d6ee3",
-        borderWidth: 2,
-        fill: true,
-        backgroundColor: gradientFill,
-        data: [3, 5, 4, 10, 8, 9, 3, 15, 14, 17],
-        pointRadius: 5,
-        pointHoverRadius: 7,
-        pointColor: "#FFFFFF",
-        fillColor: "#FFFFFF",
-        strokeColor: "#FF0000",
-
-    }]
-};
-
-var options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-        display: false
-    },
-    tooltips: {
-        mode: 'index',
-        intersect: true,
-        yPadding: 10,
-        xPadding: 10,
-        caretSize: 8,
-        backgroundColor: '#fff',
-        titleFontColor: "#6d6ee3",
-        bodyFontStyle: 'bold',
-        bodyFontColor: "#737295",
-        displayColors: false,
-        callbacks: {
-            label: function(tooltipItems, data) {
-                return "10.5";
-            }
-        },
-        bevelWidth: 3,
-        bevelHighlightColor: 'rgba(255, 255, 255, 0.75)',
-        bevelShadowColor: 'rgba(0, 0, 0, 0.5)'
-    },
-    showAllTooltips: true,
-    scales: {
-        yAxes: [{
-            display: true,
-            ticks: {
-                maxTicksLimit: 5,
-                min: 0,
-            },
-            gridLines: {
-                display: true
-            }
-        }],
-        xAxes: [{
-            display: true,
-            ticks: {
-                fontSize: 12,
-                fontColor: '#c3c6de'
-            },
-            gridLines: {
-                display: false
-            }
-        }]
-    },
-    elements: {
-        point: {
-            radius: 0
-        }
-    }
-};
-
-Chart.pluginService.register({
-    beforeRender: function(chart) {
-        if (chart.config.options.showAllTooltips) {
-            // create an array of tooltips
-            // we can't use the chart tooltip because there is only one tooltip per chart
-            chart.pluginTooltips = [];
-            chart.config.data.datasets.forEach(function(dataset, i) {
-                chart.getDatasetMeta(i).data.forEach(function(sector, j) {
-                    chart.pluginTooltips.push(new Chart.Tooltip({
-                        _chart: chart.chart,
-                        _chartInstance: chart,
-                        _data: chart.data,
-                        _options: chart.options.tooltips,
-                        _active: [sector]
-                    }, chart));
-                });
-            });
-
-            // turn off normal tooltips
-            chart.options.tooltips.enabled = false;
-        }
-    },
-    afterDraw: function(chart, easing) {
-        if (chart.config.options.showAllTooltips) {
-            // we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
-            if (!chart.allTooltipsOnce) {
-                if (easing !== 1)
-                    return;
-                chart.allTooltipsOnce = true;
-            }
-
-            // turn on tooltips
-            chart.options.tooltips.enabled = true;
-            Chart.helpers.each(chart.pluginTooltips, function(tooltip, i) {
-                if (i !== 3) return;
-                tooltip.initialize();
-                tooltip.update();
-                // we don't actually need this since we are not animating tooltips
-                tooltip.pivot();
-                tooltip.transition(easing).draw();
-            });
-            chart.options.tooltips.enabled = false;
-        }
-    }
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
 });
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
-});
+
+
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+
+
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+
+
+
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
